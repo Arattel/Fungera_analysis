feeaefb1e1264622f9760d9492261b2c9806ab83
Arattel <oleksandr@nlplab.ai>
2021-05-31 14:00:58 +0300

    Bugfix

--- conf/config.py
+++ conf/config.py
@@ -8,6 +8,13 @@ default_ancestors: Dict = {
     'error_correction': 'initial_interpreted.gen'
 }
 
+default_cache_files: Dict[str, str] = {
+    'base': 'data/base_fungera_cache.pkl',
+    'jump_directed': 'data/jump_directed_cache.pkl',
+    'jump_direction_independent': 'data/jump_direction_independent_cache.pkl',
+    'error_correction': 'data/error_correction_cache.pkl'
+}
+
 
 @dataclass
 class Config:
@@ -22,13 +29,15 @@ class Config:
     kill_organisms_ratio: float = 0.5
     memory_full_ratio: float = 0.90
     cycle_gap: int = 10
-    random_rate: int = 2000
+    random_rate: int = 400
     stack_length: int = 8
-    organism_death_rate: int = 100
+    organism_death_rate: int = 10
     kill_if_no_child: int = 25000
-    autosave_rate: Tuple = (40, 1)
+    autosave_rate: Tuple = (75, 1)
     penalize_parasitism: int = 100
     random_seed: int = 38
-    use_mutations: bool = False
+    use_mutations: bool = True
     dump_full_snapshots: bool = True
-    simulation_name: str = 'fungera_direction_independent_fixed_seed_38'
+    simulation_name: str = 'perkele!'
+    max_derivative_points: int = 15
+    smoothing_window: int = 40
